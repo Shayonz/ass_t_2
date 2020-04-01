b8c1694f7b2ce50a8d0246d38611c394d4b93905
Shayon <shayonz96@gmail.com>
2020-04-01 18:32:26 -0400

    Formatting

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Looping through images */
 
 for (let i = 1; i <= 5; i++) {
-    const newImage = document.createElement('img');
+  const newImage = document.createElement("img");
 }
 
-newImage.setAttribute('src', xxx);
+newImage.setAttribute("src", xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
-btn.onclick = function(){
-    const btnColor = btn.getAttribute("class");
+btn.onclick = function() {
+  const btnColor = btn.getAttribute("class");
 
-    if(btnColor == "dark"){
-        btn.setAttribute("class", "light");
-        btn.innerText = "lighten";
-        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
-
-    }
-    else{
-        btn.setAttribute("class", "dark");
-        btn.innerText = "darken";
-        overlay.style.backgroundColor = "rgba(0,0,0,0)";
-    }
-}
+  if (btnColor == "dark") {
+    btn.setAttribute("class", "light");
+    btn.innerText = "lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.innerText = "darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+  }
+};
