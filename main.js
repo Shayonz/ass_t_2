d7ed7ee6b71bcd37ea2163adb350247c7ba62aa8
Shayon <shayonz96@gmail.com>
2020-04-01 18:35:20 -0400

    Formatting

--- main.js
+++ main.js
@@ -7,24 +7,15 @@ const overlay = document.querySelector(".overlay");
 /* Looping through images */
 
 for (let i = 1; i <= 5; i++) {
-<<<<<<< HEAD
   const newImage = document.createElement("img");
-}
-
-newImage.setAttribute("src", xxx);
-thumbBar.appendChild(newImage);
-
-=======
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'imgs/pic' + i + '.jpg');
-    thumbBar.appendChild(newImage);
-    newImage.onclick = function(x) {
-        //https://developer.mozilla.org/en-US/docs/Web/API/Event/target
+  newImage.setAttribute("src", "imgs/pic" + i + ".jpg");
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(x) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/Event/target
     displayedImage.src = x.target.src;
-    };
+  };
 }
 
->>>>>>> e8051dc362aab1a060858392ab49b35ffa6324d0
 /* Wiring up the Darken/Lighten button */
 btn.onclick = function() {
   const btnColor = btn.getAttribute("class");
