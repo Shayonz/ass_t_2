d5d6f62d36ff375a53b8353a7d7ea0f65d0c6e10
sarwat kazmi <snkazmi27@gmail.com>
2020-04-01 18:31:03 -0400

    adding function for onclick image display

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ const overlay = document.querySelector('.overlay');
 for (let i = 1; i <= 5; i++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', 'imgs/pic' + i + '.jpg');
-
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(x) {
+        //https://developer.mozilla.org/en-US/docs/Web/API/Event/target
+    displayedImage.src = x.target.src;
+    };
 }
-
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
